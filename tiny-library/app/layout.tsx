454523d6c73d89403572e265800f92ade40b7fa5
bhashlamoon <bara1brh@gmail.com>
2026-07-04 16:40:26 +0300

    homepage structure and styling

--- tiny-library/app/layout.tsx
+++ tiny-library/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html
       lang="en"
     >
-      <body className={`${nunitoSans.className} ${lato.variable}`}>
+      <body className={`${nunitoSans.className} ${lato.variable} `}>
         <Navbar/>
         {children}
         </body>
--- tiny-library/app/page.tsx
+++ tiny-library/app/page.tsx
@@ -1,8 +1,14 @@
 
+import Image from 'next/image'
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="flex flex-col justify-center h-screen my-6 ">
+      <div className="space-y-6 flex-1 mx-5">
+        <h1 className="text-[50px] font-bold leading-15">Find your next favourite book</h1>
+        <p className="text-[23px]" >A cosy corner of the web where readers discover hand‑picked titles across every genre, from timeless classics to hidden indie gems.</p>
+        <Image src={'/hero-image.png'} width={1798} height={1799} alt='hero home page image' />
+      </div>
+
+    </main>
   );
 }
