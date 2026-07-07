64fec5bd84b997c667b8e43f97822b021aa55f6c
bhashlamoon <bara1brh@gmail.com>
2026-07-07 17:04:53 +0300

    search for book function

--- tiny-library/app/components/booksGrid.tsx
+++ tiny-library/app/components/booksGrid.tsx
@@ -2,15 +2,18 @@
 import BookCard from "./bookCard"
 import { getBooks } from '@/app/utils/books'
 import { useSearchParams } from 'next/navigation'
+import { Search } from 'lucide-react';
+import Form from 'next/form'
 
 
 export default function BooksGrid() {
 
     const searchParams = useSearchParams()
-    const category = searchParams.get('category')
-    
-    const data = getBooks(category||undefined)
-    const booksData = data.map(
+    const category = searchParams.get("category") ?? undefined;
+    const search = searchParams.get("search") ?? undefined;
+
+    const books = getBooks(category, search);
+    const booksData = books.map(
         (book) => <BookCard
             key={book.id}
             id={book.id}
@@ -19,7 +22,17 @@ export default function BooksGrid() {
             author={book.author}
             category={book.category}
             likes={book.likes} />)
-    return <div className="grid grid-cols-2 gap-3 p-3">
-        {booksData}
+    return <div className="flex flex-col">
+        <div className="py-3 px-15">
+            <div className="rounded-2xl flex gap-4 py-1 px-3 border-1 border-gray-300">
+                <Search color="#bfc3c9" />
+                <Form action='/books?search'>
+                    <input autoComplete="off" className="border-none focus:outline-hidden" type="text" name="search" placeholder="Search for any book ..." />
+                </Form>
+            </div>
+        </div>
+        <div className="grid grid-cols-2 gap-3 p-3">
+            {booksData}
+        </div>
     </div>
 }
--- tiny-library/app/utils/books.tsx
+++ tiny-library/app/utils/books.tsx
@@ -1,4 +1,4 @@
-import { DatabaseArrowDown } from "lucide-react";
+import { DatabaseArrowDown, Search } from "lucide-react";
 import { DataType } from "../types";
 import {data} from '@/app/data/data'
 
@@ -7,10 +7,13 @@ export function getBookDetails(bookId:number) {
         return bookDetails[0]
     }
 
-export function getBooks(category:string|undefined){
+export function getBooks(category?:string,search?:string){
     let filteredBooks = [...data]
     if(category){
         filteredBooks = data.filter((book)=>book.category==category)
+    }
+    if(search){
+        filteredBooks= data.filter((book)=>(book.name.toLowerCase().includes(search.toLowerCase())))
     }
     return filteredBooks
 
