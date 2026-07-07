e0f56306750e283d35254c20a6ae882d9235661e
bhashlamoon <bara1brh@gmail.com>
2026-07-07 18:25:53 +0300

    about page

---
+++ tiny-library/app/about/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { Layers, Globe, Flag } from 'lucide-react';
+
+export default function About() {
+    return <div className='flex flex-col gap-3 p-6'>
+        <Image src={'/hero-image-square.png'} className='max-h-auto object-cover w-full ' alt='card image' width={1799} height={1799}></Image>
+        <p className='uppercase text-gray-500 text-sm'>About Tiny Library</p>
+        <h1 className='text-3xl font-bold'>Small shelf, big impact</h1>
+        <p>Tiny Library started as a simple idea: make it easier for curious readers to actually find books
+            they’ll love, not just scroll
+            endless lists. Every title here is chosen with care, not algorithms.</p>
+        <hr className="border-t border-gray-300"></hr>
+        <h1 className='flex gap-3 items-center text-2xl font-bold'><Layers /> Curated, not crowded</h1>
+        <p>Tiny Library keeps the catalogue intentionally small so every book feels like a recommendation.</p>
+        <h1 className='flex pt-4 gap-3 items-center text-2xl font-bold'><Globe />Easy to browse</h1>
+        <p>Clear categories and simple descriptions make it quick to choose what you actually want to read next.</p>
+        <h1 className='flex gap-3 pt-4 items-center text-2xl font-bold'><Flag />Readers first</h1>
+        <p>Every part of Tiny Library is designed to help you spend less time searching and more time reading.</p>
+        <Image  loading="eager" src="/tinylibrary-logo.png" width={200} height={200} 
+                alt='tiny-library website logo'
+                 className='items-center mx-auto' />
+
+    </div>
+}
--- tiny-library/app/components/navbar.tsx
+++ tiny-library/app/components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image';
+import Link from 'next/link';
 import NavLink from '@/app/components/navLink'
 import { usePathname } from 'next/navigation'
 import type { ReactElement } from 'react';
@@ -7,9 +8,9 @@ export default function Navbar(): ReactElement {
     const pathName = usePathname()
    
     return <div className='bg-white flex flex-row justify-between px-4'>
-        <Image  loading="eager" src="/tinylibrary-logo.png" width={200} height={200} 
+        <Link href={'/'}><Image  loading="eager" src="/tinylibrary-logo.png" width={200} height={200} 
         alt='tiny-library website logo'
-         className='w-20 h-auto'  />
+         className='w-20 h-auto'  /></Link>
         <ul className='flex items-center gap-1.5'>
             
         <NavLink href='/books' isActive={pathName=='/books'}>BOOKS</NavLink>
