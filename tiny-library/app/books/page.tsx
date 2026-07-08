2671973108622fd05b59fee903b37d040eba991d
bhashlamoon <bara1brh@gmail.com>
2026-07-08 15:50:07 +0300

    fix building issues

--- tiny-library/app/books/page.tsx
+++ tiny-library/app/books/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import BookCategories from '../components/bookCategories'
+import { Suspense } from "react";
 import BooksGrid from '../components/booksGrid'
 export default function Books() {
     return <>
     <BookCategories/>
-    <BooksGrid/>
+   <Suspense fallback={<div>Loading...</div>}>
+      <BooksGrid />
+    </Suspense>
     </>
 }
